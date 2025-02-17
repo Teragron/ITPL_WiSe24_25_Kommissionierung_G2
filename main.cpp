761159d9813ba5d7af0103d503d7bbd429f3d884
Teragron <54573938+Teragron@users.noreply.github.com>
2025-02-18 00:03:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include "SimulationsGUI2.hpp"
 #include <iostream>
 int main() {
-    try {                                                                                                                   // [1] 
+    try {                                                                                                                           // [1] 
         SimulationsGUI Simulation;
-        Simulation.starteSimulationsGUI();                                      // Starte Simulation, beginnend mit der GUI
+        Simulation.starteSimulationsGUI();   // Starte Simulation, beginnend mit der GUI
     }
-    catch (const std::exception& e) {                                                                                      // [1], [3]
-        std::cerr << "Fehler: " << e.what() << std::endl;                                                                  // [2] 
+    catch (const std::exception& e) {                                                                                              // [1], [3]
+        std::cerr << "Fehler: " << e.what() << std::endl;                                                                          // [2] 
         return 1;
     }
     catch (...) {
